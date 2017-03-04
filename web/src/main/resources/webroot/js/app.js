0a7dfda128fe687edddead562b2adbdb9299f11a
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2017-03-04 19:54:01 +0300

    Added stop button to the feed.html with implementation in app.js, added some style to the style.css(twit stream on the right column)

--- web/src/main/resources/webroot/js/app.js
+++ web/src/main/resources/webroot/js/app.js
@@ -1,23 +1,33 @@
-function send() {
+'use strict';
+
+var localUrl = 'http://localhost:8888/';
+
+function startStreaming() {
     var filters = $('#filter').val();
     if (filters.length > 0) {
-        var url = 'http://localhost:8888/start&filter=' + encodeURIComponent(filters);
-        $.get(url);
+        $.get(localUrl + 'start&filter=' + encodeURIComponent(filters));
         document.location.href = 'feed.html', true;
     }
 }
 
+function stopStreaming() {
+    $.get(localUrl + 'stop');
+    document.location.href = '/', true;
+}
+
 var eb = new vertx.EventBus("/eventbus");
-eb.onclose = function () {
-    console.log("closing socket");
-    eb = null;
-};
+
 eb.onopen = function () {
     console.log("opening the socket");
     eb.registerHandler("stream", function (msg) {
         console.log("receiving the message " + msg);
         var message = JSON.parse(msg);
         $("#messages").prepend(
-            "<p>" + message.text + "</p>");
+            "<p>" + message.user.name + ': ' + message.text + "</p>");
     });
+};
+
+eb.onclose = function () {
+    console.log("closing socket");
+    eb = null;
 };
