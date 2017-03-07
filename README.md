7123c0421304e929431b13f1fd4e67a1e8bff14c
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2017-03-07 20:23:27 +0300

    Add description to the README.md and some style refactoring

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-#This will be a project using Spark, Vertx, Docker on microservices architecture
+#Twitter streaming
 
+This is a simple project using Java, Spark, Vertx, Docker and the
+ architecture of microservices.
+
+This application simply receives a filtered tweet from the twitter stream
+ from Spark's TwitterUtils and outputs the received tweets to the HTML page.
+
+Note you need installed Docker and Maven for run this app.
+
+To run this application from terminal, you need to move it to the root "produser"
+ folder and execute: 
+ 
+ __mvn clean package -P con,run__
+ 
+ Then in another terminal window repeate this command in the root of
+  "web" folder.
+  
+ If you use Unix, you can also run the program using a __runapp.sh__ script.
+ 
+ Note that this script stops all running Docker containers and can delete some
+  images, check before using.
+ 
+ When the application start, go to [http://localhost:8080](http://localhost:8080)
+ enter some keywords or hashtags to the input form and press __Start__.
+ 
+ For stop app press __Stop__ button and you back to the start page.
--- web/src/main/resources/webroot/js/app.js
+++ web/src/main/resources/webroot/js/app.js
@@ -3,31 +3,31 @@
 var localUrl = 'http://localhost:8888/';
 
 function startStreaming() {
-    var filters = $('#filter').val();
-    if (filters.length > 0) {
-        $.get(localUrl + 'start&filter=' + encodeURIComponent(filters));
-        document.location.href = 'feed.html', true;
-    }
+  var filters = $('#filter').val();
+  if (filters.length > 0) {
+    $.get(localUrl + 'start&filter=' + encodeURIComponent(filters));
+    document.location.href = 'feed.html', true;
+  }
 }
 
 function stopStreaming() {
-    $.get(localUrl + 'stop');
-    document.location.href = '/', true;
+  $.get(localUrl + 'stop');
+  document.location.href = '/', true;
 }
 
 var eb = new vertx.EventBus("/eventbus");
 
 eb.onopen = function () {
-    console.log("opening the socket");
-    eb.registerHandler("stream", function (msg) {
-        console.log("receiving the message " + msg);
-        var message = JSON.parse(msg);
-        $("#messages").prepend(
-            "<p>" + message.user.name + ': ' + message.text + "</p>");
-    });
+  console.log("opening the socket");
+  eb.registerHandler("stream", function (msg) {
+    console.log("receiving the message " + msg);
+    var message = JSON.parse(msg);
+    $("#messages").prepend(
+        "<p>" + message.user.name + ': ' + message.text + "</p>");
+  });
 };
 
 eb.onclose = function () {
-    console.log("closing socket");
-    eb = null;
+  console.log("closing socket");
+  eb = null;
 };
